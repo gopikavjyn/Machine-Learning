907c8a5db8bbeac402d50e874dbc8cd06bfbd649
gopikavjyn <94525918+gopikavjyn@users.noreply.github.com>
2022-08-29 11:32:55 +0530

    commit

---
+++ isoscles.py
@@ -0,0 +1,12 @@
+print("20 Gopika Vijayan ")
+n1=int(input("enter the side of triangle:"))
+n2=int(input("enter the side of triangle:"))
+n3=int(input("enter the side of triangle:"))
+if n1==n2 and n2==n3:
+   print("triangle is equilateral")
+elif n1==n2 or n2==n3 or n1==n3:
+   print("triangle is isoscless")
+else:
+   print("traingle is scalar")
+
+
