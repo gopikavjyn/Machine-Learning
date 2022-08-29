81ec995ed081dd4aecf438a3619dac8833b0ca6b
gopikavjyn <94525918+gopikavjyn@users.noreply.github.com>
2022-08-29 12:03:24 +0530

    commit

---
+++ qn8.py
@@ -0,0 +1,8 @@
+print("20 Gopika Vijayan ")
+list1=['sunday','monday','tuesday','wednesday','thursday','friday','saturday']
+tuple1=('sunday','monday','tuesday','wednesday','thursday','friday','saturday')
+set1={'sunday','monday','tuesday','wednesday','thursday','friday','saturday'}
+dict1={'1':'sunday','2':'monday','3':'tuesday','4':'wednesday','5':'thursday','6':'friday','7':'saturday'}
+print(list1,type(list1))
+print(tuple1,type(tuple1))
+print(dict1,type(dict1))
