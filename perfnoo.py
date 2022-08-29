22c7c2486bb5b91de5facc5cf25aa47e64c5d257
gopikavjyn <94525918+gopikavjyn@users.noreply.github.com>
2022-08-29 11:44:45 +0530

    commit

---
+++ perfnoo.py
@@ -0,0 +1,12 @@
+print("20 Gopika Vijayan ")
+n=int(input("enter the no:"))
+sum=0
+for i in range(1,n):
+   if (n % i == 0):
+       sum=sum+i
+
+if (sum == n):
+   print("perfect number")
+else:
+   print("not perfect number")
+
